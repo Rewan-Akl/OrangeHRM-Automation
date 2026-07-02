254225451ede69d8e93ac538a1d1d7c52e5227f6
Rewan Wael Akl <132017721+RewanWael@users.noreply.github.com>
2026-07-02 22:23:14 +0300

    Implement ConfigReader

---
+++ src/main/java/com/rewan/orangehrm/utils/ConfigReader.java
@@ -0,0 +1,30 @@
+package com.rewan.orangehrm.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream input = ConfigReader.class
+                .getClassLoader()
+                .getResourceAsStream("config.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("config.properties file not found");
+            }
+
+            properties.load(input);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
